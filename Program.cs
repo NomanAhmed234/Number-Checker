557d98d087ad364cabb5fb20c18e7c9a694433ec
Noman Ahmed <nomanpb234@gmail.com>
2023-07-16 16:12:46 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab_2a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The program that checks that either the user input number is EVEN, ODD or ZERO.
+            Console.WriteLine("Enter the number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number  == 0)
+            {
+                Console.WriteLine("The number is Zero.");
+            }
+            else if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is Even.");
+            }
+            else  
+            {
+                Console.WriteLine("The number is Odd.");
+            }
+             
+        }
+    }
+}
